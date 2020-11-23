265fa23470cccc21967a78b31907c026b4466610
Julien Dargelos <julien.dargelos@me.com>
2020-11-23 19:43:51 +0100

    feat: create release action

---
+++ readme.md
@@ -0,0 +1,59 @@
+# Release Action
+
+> GitHub Action to automatically release packages using lerna or semantic-release.
+
+- Automatically bump packages version using [semantic-release](https://github.com/semantic-release/semantic-release) (or [lerna](https://github.com/lerna/lerna) which also relies on semantic-release)
+- Create releases on GitHub
+- Publish to GitHub and NPM registries
+
+Publishing on package registries is skipped if there is not any `package.json` or if its `private` field is set to true.
+
+Publishing on GitHub registry is skipped if the package name is not [scoped with the GitHub owner name](https://docs.github.com/en/free-pro-team@latest/packages/using-github-packages-with-your-projects-ecosystem/configuring-npm-for-use-with-github-packages#publishing-a-package).
+
+Publishing on NPM registry is skipped if no [NPM token](https://docs.npmjs.com/about-access-tokens) is provided.
+
+## Usage
+
+In `.github/workflows/release.yml`:
+
+```yaml
+on:
+  push:
+    branches:
+    - main
+
+jobs:
+  release:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v2
+    - uses: actions/setup-node@v1
+      with:
+        node-version: 12
+    - run: yarn
+    - run: yarn build
+    - uses: plutotcool/release@v1
+      with:
+        github_token:  ${{ secrets.GITHUB_TOKEN }}
+        npm_token: ${{ secrets.NPM_TOKEN }}
+```
+
+## Inputs
+
+### `github_token`
+
+`string` *(required)*
+
+[GitHub Token](https://docs.github.com/en/free-pro-team@latest/actions/reference/authentication-in-a-workflow#about-the-github_token-secret) used to create the release on GitHub and publish on GitHub registry.
+
+### `npm_token`
+
+`string` *(optional)*
+
+[NPM token](https://docs.npmjs.com/about-access-tokens) with publish permission. If not provided, publishing to npm registry will be skipped.
+
+### `publish`
+
+`string` *(optional, default `true`)*
+
+Enable or disable publishing to package registries. When set to false, only releases are created on GitHub.
---
+++ src/index.ts
@@ -0,0 +1,185 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as github from '@actions/github'
+import { promises as fs } from 'fs'
+
+(async () => {
+  try {
+    const owner: string = github.context.repo.owner
+    const ownerScope: string = `@${owner}`
+    const npmToken: string = core.getInput('npm_token')
+    const githubToken: string = core.getInput('github_token', { required: true })
+    const publish: boolean = core.getInput('publish') !== 'false'
+
+    let publishToGithub: boolean
+    let publishToNPM: boolean
+    let privatePackage: boolean
+    let scope: string
+    let cli: string
+    let cliPath: string
+    let release: (
+      path: string,
+      release: boolean,
+      publish: boolean,
+      env?: { [variable: string]: string }
+    ) => Promise<void>
+
+    try {
+      const pkg = JSON.parse((await fs.readFile('package.json')).toString())
+      privatePackage = pkg.private || false
+      scope = pkg.name.slice(0, pkg.name.indexOf('/'))
+      publishToGithub = publish && !privatePackage && scope === ownerScope
+      publishToNPM = publish && !privatePackage && !!npmToken
+    } catch (_) {
+      privatePackage = true
+      scope = ownerScope
+      publishToGithub = false
+      publishToNPM = false
+    }
+
+    try {
+      await fs.access('lerna.json')
+      cli = 'lerna'
+      release = lernaRelease
+
+      core.info(
+        'Lerna detected, releasing using lerna'
+      )
+    } catch(_) {
+      cli = 'semantic-release'
+      release = semanticRelease
+
+      core.info(
+        'Lerna not detected, releasing using semantic-release'
+      )
+    }
+
+    if (!publish) {
+      core.info(
+        'Publishing disabled, skipping publishing to package registries'
+      )
+    } else if (privatePackage) {
+      core.info(
+        'Private package detected, skipping publishing to package registries'
+      )
+    } else {
+      scope !== ownerScope && core.warning(
+        `Package not scoped with ${ownerScope}, skipping publishing to GitHub registry`
+      )
+
+      !npmToken && core.warning(
+        'NPM token not provided, skipping publishing to NPM registry'
+      )
+    }
+
+    try {
+      await fs.access(`node_modules/${cli}/package.json`)
+    } catch (_) {
+      core.info(
+        `Installing ${cli}...`
+      )
+
+      await exec.exec('npm', [
+        'install',
+        cli,
+        '--no-save',
+        '--no-package-lock'
+      ])
+
+      core.info(
+        `Installed ${cli}`
+      )
+    }
+
+    cliPath = `node_modules/${cli}/${JSON
+      .parse((await fs.readFile(`node_modules/${cli}/package.json`)).toString())
+      .bin[cli]
+    }`
+
+    core.info(
+      `Creating release on GitHub${publishToGithub ? ' and publishing to GitHub registry' : ''}...`
+    )
+
+    await release(cliPath, true, publishToGithub, {
+      NPM_CONFIG_REGISTRY: `https://npm.pkg.github.com/${owner}`,
+      NPM_TOKEN: githubToken,
+      GITHUB_TOKEN: githubToken
+    })
+
+    core.info(
+      'Release available on GitHub'
+    )
+
+    publishToGithub && core.info(
+      'Package available on GitHub registry'
+    )
+
+    publishToNPM && core.info(
+      'Publishing to NPM registry...'
+    )
+
+    await release(cliPath, false, publishToNPM, {
+      NPM_CONFIG_REGISTRY: 'https://registry.npmjs.org',
+      NPM_TOKEN: npmToken
+    })
+
+    publishToNPM && core.info(
+      'Package available on NPM registry'
+    )
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+})()
+
+async function lernaRelease(
+  path: string,
+  release: boolean,
+  publish: boolean,
+  env: { [variable: string]: string } = {}
+): Promise<void> {
+  if (release) {
+    await exec.exec('node', [
+      path,
+      'version',
+      '--yes',
+      '--conventional-commits'
+    ], { env })
+  }
+
+  if (publish) {
+    await exec.exec('node', [
+      path,
+      'publish',
+      '--yes'
+    ], { env })
+  }
+}
+
+async function semanticRelease(
+  path: string,
+  release: boolean,
+  publish: boolean,
+  env: { [variable: string]: string } = {}
+): Promise<void> {
+  if (release) {
+    await exec.exec('node', [
+      path,
+      '--no-ci',
+      '--extends',
+      `${__dirname}/../release.config.js`,
+      '--plugins',
+      '@semantic-release/commit-analyzer,' +
+      '@semantic-release/release-notes-generator,' +
+      '@semantic-release/github'
+    ], { env })
+  }
+
+  if (publish) {
+    await exec.exec('node', [
+      path,
+      '--no-ci',
+      '--extends',
+      `${__dirname}/../release.config.js`
+    ], { env })
+  }
+}
