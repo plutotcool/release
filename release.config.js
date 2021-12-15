872c099bf85ed51b70873f311376643c3261d30c
Julien Dargelos <julien.dargelos@me.com>
2021-12-15 12:46:33 +0100

    fix: explicit plugins option

--- release.config.js
+++ release.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github'
+  ],
   branches: [
     '+([0-9])?(.{+([0-9]),x}).x',
     'main',
